b923472f7f54c198121d954088f25f339c24a505
Dmitriy Lunev <dima.lunev14@gmail.com>
2025-04-04 22:07:12 +0300

    Init nvim configs

---
+++ lua/community.lua
@@ -0,0 +1,12 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.hyprlang" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+}
